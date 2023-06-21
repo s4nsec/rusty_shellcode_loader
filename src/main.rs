b8ff48e6a9b5df376e69214bb7066d1f6d3df88b
Sanan Hasanov <s4nsec@github.com>
2023-06-21 17:37:51 -0400

    Rewriting shellcoded loader in rust. Added VirtualAlloc first.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate libloading;
+use std::os::raw::{c_void, c_ulong};
+use libloading::{Library, Symbol};
+
+pub type pVirtualAlloc = unsafe extern "system" fn(
+    lpAddress: *mut c_void,
+    dwSize: usize,
+    flAllocationType: c_ulong,
+    flProtect: c_ulong,
+) -> *mut c_void;
+
+fn main() {
+    let lib = Library::new("kernel32.dll").expect("Couldn't load the kernel32.dll");
+
+    let virtual_alloc: Symbol<VirtualAllocFn> = unsafe {
+        lib.get(b"VirtualAlloc\0").expect("Couldn't find the address of VirtualAlloc");
+
+    }
+    let size: usize = 1024;
+
+    let return_address = unsafe {
+        virtual_alloc(
+            std::ptr::null_mut(),
+            size,
+            0x1000,
+            0x04
+        )
+    };
+
+    if return_address.is_null() {
+        println!("Allocation failed");
+    }
+    else{
+        println!("Allocation succeeded. The address allocated: {}", return_address);
+    }
+
+}
